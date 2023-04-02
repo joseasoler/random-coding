449b13a1f882412dc5e4016daaaa28bc54fbff85
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-04-02 20:30:46 +0200

    Mergesort

---
+++ cmake/clang_tidy.cmake
@@ -0,0 +1,25 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+include_guard(GLOBAL)
+
+option(RANDOM_CODING_CLANG_TIDY "Analyze the project with clang-tidy." ON)
+
+if (RANDOM_CODING_CLANG_TIDY)
+	find_program(
+		CLANG_TIDY_BINARY
+		NAMES "clang-tidy"
+		DOC "Clang-tidy binary"
+	)
+
+	if (CLANG_TIDY_BINARY)
+		message(STATUS "Found clang-tidy binary - ${CLANG_TIDY_BINARY}")
+		set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_BINARY}"
+			"-extra-arg=-Wno-unknown-warning-option" # Ignore GCC-only warnings.
+			"-warnings-as-errors=*"
+			)
+	else ()
+		message(WARNING "clang-tidy binary not found")
+	endif ()
+endif ()
---
+++ src/20230402.cpp
@@ -0,0 +1,70 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <algorithm>
+#include <cassert>
+#include <limits>
+#include <random>
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+
+using vector = std::vector<int>;
+
+void mergesort(vector::iterator begin, vector::iterator end) {
+	assert(begin < end);
+	const auto size = std::distance(begin, end);
+	assert(size > 0UL);
+	if (size == 1UL) { return; }
+	if (size == 2UL && (*begin > *(begin + 1UL))) {
+		std::iter_swap(begin, begin + 1UL);
+		return;
+	}
+	const auto half_size = size / 2UL;
+	assert(size - half_size > 0UL);
+	const auto middle = begin + half_size;
+	mergesort(begin, middle);
+	mergesort(middle, end);
+
+	// Extra space for sorting.
+	const vector copy(begin, end);
+	const auto copy_begin = copy.cbegin();
+	const auto copy_middle = copy_begin + half_size;
+	const auto copy_end = copy.cend();
+
+	auto output = begin;
+	auto first_half = copy_begin;
+	auto second_half = copy_middle;
+
+	while (output != end) {
+		if (second_half == copy_end || (*first_half <= *second_half && first_half < copy_middle)) {
+			*output = *first_half;
+			++first_half;
+		} else {
+			*output = *second_half;
+			++second_half;
+		}
+		assert(first_half <= second_half);
+		++output;
+	}
+	assert(first_half == copy_middle);
+	assert(second_half == copy_end);
+}
+
+TEST_CASE("20230402") {
+	constexpr std::size_t size = 10UL;
+
+	std::mt19937 rng{}; // NOLINT
+	std::uniform_int_distribution<int> dist(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+
+	for (std::size_t iteration = 0UL; iteration < 1000UL; ++iteration) {
+		vector reference_vector(size);
+		std::generate(reference_vector.begin(), reference_vector.end(), [&dist, &rng]() { return dist(rng); });
+		vector tested_vector(reference_vector);
+
+		std::sort(reference_vector.begin(), reference_vector.end());
+		mergesort(tested_vector.begin(), tested_vector.end());
+		REQUIRE(reference_vector == tested_vector);
+	}
+}
